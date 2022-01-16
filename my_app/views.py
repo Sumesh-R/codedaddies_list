a6394d110cb50196b639b60b504d8b2e8f2cf088
Sumesh-R <rsumesh2020@gmail.com>
2022-01-16 23:51:19 +0530

    add frontend and requirements file

--- my_app/views.py
+++ my_app/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html', {})
+    context = {
+        
+    }
+    return render(request, 'base.html', context)
+
+def new_search(request):
+    search = request.POST['search']
+    context = {
+        "search":search,
+    }
+    return render(request, 'my_app/new_search.html',context)
